be0638f9870f35571e609aabeb7ce51932fd07a6
Mauli Prakash <mprak@travisperkins.co.uk>
2019-02-18 16:51:25 +0000

    removed tmp files

--- learnpythonthehardwayorg/ex.py/ex33_old.py
+++ learnpythonthehardwayorg/ex.py/ex33_old.py
@@ -1,16 +1,34 @@
+#!/bin/python
 i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
+while i < 10:
+    print (" i is at the value %r") %i
     numbers.append(i)
-
     i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+    print (" i is now at value %r") %i
+
+print "The numbers are :"
+
+for num in numbers:
+    print ("%r") %num
 
 
-print "The Numbers: "
+
+# rewrite as function
+countto = input("Enter the number to count to : ")
+numbers = []
+def count(countto):
+   """This is a rebuild using functions"""
+   i = 0 
+   while i < countto:
+       print("At the top i is %r") %i
+       numbers.append(i)
+       i = i + 1
+       print ("At the bottom i is %r") %i
+
+count(countto)
+print ("The numbers are : -")
 
 for num in numbers:
     print num
